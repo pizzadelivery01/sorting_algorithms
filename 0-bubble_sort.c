76c9b57d2e9f1abf3dc5182132441c143f1867fb
Jeremy Bedolla <jjbedolla@gmail.com>
2020-02-03 22:53:57 +0000

    betty fixes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,51 +4,48 @@
 
 /**
  * bubble_sort - soriting algorithm that brings biggest to top
- * array: pionter to array head
- * size: elements in array
+ * @array: pionter to array head
+ * @size: elements in array
  * return: nothing
  */
 
 void bubble_sort(int *array, size_t size)
 {
 
-  size_t i, j;
-  bool swapped;
-  
+	size_t i, j;
+	bool swapped;
 
-  if (array != NULL && size >= 2)
-    
-
-    for (i = 0; i < (size-1); i++)
-      {
-	swapped = false;
-	for (j = 0; j < (size-i)-1; j++)
-	  {
-	    if (array[j] > array[j+1])
-	      {
-		swap(&array[j], &array[j+1]);
-		print_array(array, size);
-		swapped = true;
-	      }
-	  }
-	if (swapped == false)
-	  break;
-      }
+	if (array != NULL && size >= 2)
+		for (i = 0; i < (size - 1); i++)
+		{
+			swapped = false;
+			for (j = 0; j < (size - i) - 1; j++)
+			{
+				if (array[j] > array[j + 1])
+				{
+					swap(&array[j], &array[j + 1]);
+					print_array(array, size);
+					swapped = true;
+				}
+			}
+			if (swapped == false)
+				break;
+		}
 }
 
 /**
  * swap - swaps to nodes
- * node1: first node
- * node2: second node
+ * @node1: first node
+ * @node2: second node
  * return: nothing
  */
 
 void swap(int *node1, int *node2)
 {
-  int temp;
+	int temp;
 
 
-  temp = *node1;
-  *node1 = *node2;
-  *node2 = temp;
+	temp = *node1;
+	*node1 = *node2;
+	*node2 = temp;
 }
