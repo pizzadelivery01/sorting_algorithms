0b162c8a138571fe3ec7db3a528bf1efc807af54
Jeremy Bedolla <jjbedolla@gmail.com>
2020-02-03 21:59:21 +0000

    added swap function to bubble file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -30,3 +30,20 @@ void bubble_sort(int *array, size_t size)
 	break;
     }
 }
+
+/**
+ * swap - swaps to nodes
+ * node1: first node
+ * node2: second node
+ * return: nothing
+ */
+
+void swap(int *node1, int *node2)
+{
+  int temp;
+
+
+  temp = *node1;
+  *node1 = *node2;
+  *node2 = temp;
+}
